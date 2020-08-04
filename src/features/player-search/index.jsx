0f4a0aa28261f2750bdd92a649a651ae5cdb0831
Igor Tatarinov <igor.tanv@gmail.com>
2020-08-04 15:18:01 +0700

    wip

--- src/features/player-search/index.jsx
+++ src/features/player-search/index.jsx
@@ -17,31 +17,35 @@ const weightClassOptions = {
 }
 
 const giNoGiOptions = {
-  NoGi: "No Gi",
-  Gi: "Gi"
+  nogi: "No Gi",
+  gi: "Gi"
 }
 
-function search(players) {
-  return filterByWeightClass(filterByGoNoGi(players))
-}
 
-function filterByGoNoGi(players) {
-  return players
-}
-
-function filterByWeightClass(players) {
-  return players
-}
 
 export default function PlayerSearch({ }) {
   const [players, setPlayers] = useState([])
-  const [giNoGi, setGiNoGi] = useState(null)
+  const [giNoGi, setGiNoGi] = useState("nogi")
+  const [weightClass, setWeightClass] = useState("Absolute")
 
   useEffect(() => {
     apiFetch("players.json").then(json => setPlayers(json.players))
   }, [])
 
+  function search(players) {
+    return filterByWeightClass(sortByGiNoGi(players))
+  }
 
+  function sortByGiNoGi(players) {
+    return players.sort((a, b) => a[giNoGi] - b[giNoGi])
+  }
+
+  function filterByWeightClass(sorted) {
+    if (weightClass === "Absolute") return sorted
+    return sorted.filter(player => player.weightClass === weightClass)
+  }
+
+  const found = search(players)
   return <div>
     <div style={{
       display: "flex"
@@ -49,24 +53,29 @@ export default function PlayerSearch({ }) {
       <Dropdown options={Object.keys(giNoGiOptions).map(value => ({
         value, label: giNoGiOptions[value]
       }))}
-
+        onChange={val => setGiNoGi(val.value)}
+        value={giNoGi}
       />
-
       <Dropdown options={Object.keys(weightClassOptions).map(value => ({
         value, label: weightClassOptions[value]
-      }))} />
+      }))}
+        onChange={val => setWeightClass(val.value)}
+        value={weightClass}
+      />
     </div>
     <ul>
-      {search(players).map(player => {
-        const { firstName, lastName, weightClass, win, loss, draw } = player
-        return <li>
-          <div style={{ display: "flex" }}>
-            <div>{firstName} {lastName}</div>
-            <div>{weightClass}</div>
-            <div>win: {win} loss: {loss} draw: {draw}</div>
-          </div>
-        </li>
-      })}
+      {found.length > 0
+        ? (found.map(player => {
+          const { firstName, lastName, weightClass, wins, losses, draws } = player
+          return <li key={player._id}>
+            <div style={{ display: "flex" }}>
+              <div>{firstName} {lastName}</div>
+              <div>{weightClass}</div>
+              <div>win: {wins} loss: {losses} draw: {draws}</div>
+            </div>
+          </li>
+        })) : "There are no fighters in that weight class"}
+
     </ul>
   </div>
 }
