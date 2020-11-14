157e0c01cb3e9a164b05d2741c9ca4e6edfcb86e
Igor Tatarinov <igor.tanv@gmail.com>
2020-11-15 02:48:38 +0700

    added onClick

--- src/ui/button/index.jsx
+++ src/ui/button/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import "./styles.css"
 
-export default function Button({ children, disabled = false, type = "button" }) {
-  return <button disabled={disabled} className="__rival_button">{children}</button>
+export default function Button({ children, disabled = false, type = "button", onClick }) {
+  return <button onClick={onClick} disabled={disabled} className="__rival_button">{children}</button>
 }
