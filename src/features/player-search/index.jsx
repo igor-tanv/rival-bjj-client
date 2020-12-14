6f27cb407641ae0230b155a7218d8dec52930336
Vinh <vinh.giang@pushlishone.nl>
2020-12-14 20:22:43 +0700

    create player card

--- src/features/player-search/index.jsx
+++ src/features/player-search/index.jsx
@@ -1,80 +1,125 @@
-import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import Dropdown from "../../ui/dropdown"
+import Dropdown from '../../ui/dropdown';
 
-import { apiFetch } from "../../modules/api-fetch"
-import { toValueLabel } from "../../modules/object"
+import { apiFetch } from '../../modules/api-fetch';
+import { toValueLabel } from '../../modules/object';
 
-import weightClasses from "../../data/weight-classes.json"
-import matchTypes from "../../data/match-types.json"
-import communities from "../../data/communities.json"
+import weightClasses from '../../data/weight-classes.json';
+import matchTypes from '../../data/match-types.json';
+import communities from '../../data/communities.json';
 
-import Button from "../../ui/button"
-import Banner from "../../ui/banner"
+import Button from '../../ui/button';
+import Banner from '../../ui/banner';
 
-export default function PlayerSearch({ }) {
-  const [players, setPlayers] = useState([])
-  const [giNoGi, setGiNoGi] = useState(Object.keys(matchTypes)[0])
-  const [weightClass, setWeightClass] = useState(Object.keys(weightClasses)[0])
-  const [community, setCommunity] = useState(Object.keys(communities)[0])
+import './styles.css';
+
+export default function PlayerSearch({}) {
+  const [players, setPlayers] = useState([]);
+  const [giNoGi, setGiNoGi] = useState(Object.keys(matchTypes)[0]);
+  const [weightClass, setWeightClass] = useState(Object.keys(weightClasses)[0]);
+  const [community, setCommunity] = useState(Object.keys(communities)[0]);
 
   useEffect(() => {
-    apiFetch("players").then(json => setPlayers(json.players))
-  }, [])
+    apiFetch('players').then((json) => setPlayers(json.players));
+  }, []);
 
   function search(players) {
-    return sortByWeightClass(sortByGiNoGi(sortByCommunity(players)))
+    return sortByWeightClass(sortByGiNoGi(sortByCommunity(players)));
   }
 
   function sortByCommunity(players) {
-    return players.filter(player => player.community === community)
+    return players.filter((player) => player.community === community);
   }
 
   function sortByGiNoGi(playersSortedByGiNoGi) {
-    return playersSortedByGiNoGi.sort((a, b) => b[giNoGi] - a[giNoGi])
+    return playersSortedByGiNoGi.sort((a, b) => b[giNoGi] - a[giNoGi]);
   }
 
   function sortByWeightClass(communityPlayersSortedByGiNoGi) {
-    if (weightClass === "OpenWeight") return communityPlayersSortedByGiNoGi
-    return communityPlayersSortedByGiNoGi.filter(player => player.weightClass === weightClass)
+    if (weightClass === 'OpenWeight') return communityPlayersSortedByGiNoGi;
+    return communityPlayersSortedByGiNoGi.filter(
+      (player) => player.weightClass === weightClass
+    );
   }
 
-  const found = search(players)
-  return <div>
-    <Banner/>
-    <div style={{
-      display: "flex"
-    }}>
-      <Dropdown options={toValueLabel(communities)} onChange={setCommunity} value={community} />
-      <Dropdown options={toValueLabel(matchTypes)} onChange={setGiNoGi} value={giNoGi} />
-      <Dropdown options={toValueLabel(weightClasses)} onChange={setWeightClass} value={weightClass} />
-    </div>
-    <ul>
+  const found = search(players);
+  return (
+    <div>
+      <Banner />
+      <div
+        style={{
+          display: 'flex',
+        }}
+      >
+        <Dropdown
+          options={toValueLabel(communities)}
+          onChange={setCommunity}
+          value={community}
+        />
+        <Dropdown
+          options={toValueLabel(matchTypes)}
+          onChange={setGiNoGi}
+          value={giNoGi}
+        />
+        <Dropdown
+          options={toValueLabel(weightClasses)}
+          onChange={setWeightClass}
+          value={weightClass}
+        />
+      </div>
       {found.length > 0
-        ? (found.map(player => {
-          const { _id, firstName, lastName, weightClass, wins, losses, draws, avatar } = player
-          return <li key={player._id}>
-            <Link to={`/profiles/${_id}`}>
-              <div style={{ display: "flex" }}>
-                <img
-                  className="item-avatar"
-                  src={
-                    avatar
-                      ? avatar
-                      : `${process.env.PUBLIC_URL}/assets/images/cover.jpg`
-                  }
-                  alt=""
-                />
-                <div>{firstName} {lastName}</div>
-                <div>{weightClass}</div>
-                <div>win: {wins} loss: {losses} draw: {draws}</div>
-                <Button>Primary</Button>
-              </div>
-            </Link>
-          </li>
-        })) : "There are no fighters in that weight class"}
+        ? found.map((player) => {
+            const {
+              _id,
+              firstName,
+              lastName,
+              weightClass,
+              wins,
+              losses,
+              draws,
+              avatar,
+              nogi,
+              gi,
+              school,
+            } = player;
+            return (
+              <Link to={`/profiles/${_id}`}>
+                <div key={player._id} className="item-container">
+                  <img
+                    className="info-avatar"
+                    src={avatar ? avatar : `assets/images/test-image.png`}
+                    alt=""
+                  />
+                  <div className="info-wrapper">
+                    <div className="info-title">
+                      {firstName} {lastName}
+                    </div>
+                    <div className="info-weight">{weightClass}</div>
+                    <div className="info-record">
+                      Win: {wins} Loss: {losses} Draw: {draws}
+                    </div>
+                    <div className="info-rank">
+                      <span className="rank-type">Nogi Rank:</span>
+                      <span className="rank-score">#{nogi}</span>
+                    </div>
+                    <div className="info-school">
+                      <span className="school">School:</span>
+                      <span className="school-name">{school}</span>
+                    </div>
 
-    </ul>
-  </div>
+                    <Link to={`/profiles/${_id}`} className="profile-link">
+                      <img src="assets/images/profile-link-arrow.png" className="arrow"/>
+                      Visit Profile
+                    </Link>
+                    <img src="assets/images/gold.png" className="info-medal" />
+                  </div>
+                </div>
+              </Link>
+            );
+          })
+        : 'There are no fighters in that weight class'}
+    </div>
+  );
 }
--- src/ui/banner/index.jsx
+++ src/ui/banner/index.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React from 'react';
 import './styles.css';
+import Button from '../../ui/button';
 
-export default function Banner({ }) {
+export default function Banner({}) {
   return (
     <div className="banner-wrapper">
       <div className="gap"></div>
-      <img src="assets/images/neverbackdown.png" className="cover-img"/>
+      <img src="assets/images/neverbackdown.png" className="cover-img" />
+      <div className="banner-title">Challenge anyone to a grappling</div>
+      <div className="banner-sub-title">match in your area</div>
+      <div className="banner-button">
+        <Button type="submit">Learn More</Button>
+      </div>
     </div>
-  )
+  );
 }
