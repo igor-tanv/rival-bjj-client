a17ad29745f9418db5873a7195f85bed4d092523
Igor Tatarinov <igor.tanv@gmail.com>
2020-12-16 14:49:42 +0700

    fix: gi-nogi search result

--- src/features/player-search/index.jsx
+++ src/features/player-search/index.jsx
@@ -15,7 +15,7 @@ import Banner from '../../ui/banner';
 
 import './styles.css';
 
-export default function PlayerSearch({}) {
+export default function PlayerSearch({ }) {
   const [players, setPlayers] = useState([]);
   const [giNoGi, setGiNoGi] = useState(Object.keys(matchTypes)[0]);
   const [weightClass, setWeightClass] = useState(Object.keys(weightClasses)[0]);
@@ -71,54 +71,59 @@ export default function PlayerSearch({}) {
       </div>
       {found.length > 0
         ? found.map((player) => {
-            const {
-              _id,
-              firstName,
-              lastName,
-              weightClass,
-              wins,
-              losses,
-              draws,
-              avatar,
-              nogi,
-              gi,
-              school,
-            } = player;
-            return (
-              <Link to={`/profiles/${_id}`}>
-                <div key={player._id} className="item-container">
-                  <img
-                    className="info-avatar"
-                    src={avatar ? avatar : `assets/images/test-image.png`}
-                    alt=""
-                  />
-                  <div className="info-wrapper">
-                    <div className="info-title">
-                      {firstName} {lastName}
-                    </div>
-                    <div className="info-weight">{weightClass}</div>
-                    <div className="info-record">
-                      Win: {wins} Loss: {losses} Draw: {draws}
-                    </div>
+          const {
+            _id,
+            firstName,
+            lastName,
+            weightClass,
+            wins,
+            losses,
+            draws,
+            avatar,
+            nogi,
+            gi,
+            school,
+          } = player;
+          return (
+            <Link to={`/profiles/${_id}`}>
+              <div key={player._id} className="item-container">
+                <img
+                  className="info-avatar"
+                  src={avatar ? avatar : `assets/images/test-image.png`}
+                  alt=""
+                />
+                <div className="info-wrapper">
+                  <div className="info-title">
+                    {firstName} {lastName}
+                  </div>
+                  <div className="info-weight">{weightClass}</div>
+                  <div className="info-record">
+                    Win: {wins} Loss: {losses} Draw: {draws}
+                  </div>
+                  {giNoGi === 'nogi' ? <div className="info-rank">
+                    <span className="rank-type">Nogi Rank:</span>
+                    <span className="rank-score">#{nogi}</span>
+                  </div> :
                     <div className="info-rank">
-                      <span className="rank-type">Nogi Rank:</span>
-                      <span className="rank-score">#{nogi}</span>
+                      <span className="rank-type">Gi Rank:</span>
+                      <span className="rank-score">#{gi}</span>
                     </div>
-                    <div className="info-school">
-                      <span className="school">School:</span>
-                      <span className="school-name">{school}</span>
-                    </div>
+                  }
+                  <div className="info-school">
+                    <span className="school">School:</span>
+                    <span className="school-name">{school}</span>
+                  </div>
 
-                    <Link to={`/profiles/${_id}`} className="profile-link">
-                      <img src="assets/images/profile-link-arrow.png" className="arrow"/>
+                  <Link to={`/profiles/${_id}`} className="profile-link">
+                    <img src="assets/images/profile-link-arrow.png" className="arrow" />
                       Visit Profile
                     </Link>
-                    <img src="assets/images/gold.png" className="info-medal" />
-                  </div>
+                  <img src="assets/images/gold.png" className="info-medal" />
                 </div>
-              </Link>
-            );
-          })
+              </div>
+            </Link>
+          );
+        })
         : 'There are no fighters in that weight class'}
     </div>
   );
