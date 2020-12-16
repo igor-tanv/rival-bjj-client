379874c53c024c7af7b8693c0f4f32dd4ff97816
Igor Tatarinov <igor.tanv@gmail.com>
2020-12-16 15:33:47 +0700

    added old navbar for app navigation

--- src/ui/theme/header/index.jsx
+++ src/ui/theme/header/index.jsx
@@ -4,34 +4,40 @@ import "./styles.css"
 
 
 export default function Header() {
-  return (<div className="header">
-      <img src="assets/images/category.png" className="menu-icon"></img>
-      <div className="header-title">Rival</div>
-      <img src="assets/rating-stars/100.png" className="logo"></img>
-  </div >)
+  return (
+
+    <div className="container">
+      <ul className="horizontal">
+        <li><Link to="/">Home</Link></li>
+        {
+          localStorage.getItem("jwt")
+            ? <>
+              <li><a onClick={
+                () => {
+                  localStorage.removeItem("jwt")
+                  window.location = "/"
+                }
+              }>Log out</a></li>
+              <li><Link to="/profile">My Profile</Link></li>
+            </>
+            : <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </>
+        }
+
+        <li><Link to="/rules">Rules</Link></li>
+        <li><Link to="/faq">FAQ</Link></li>
+      </ul>
+    </div >
+
+
+  )
+
+  //   (<div className="header">
+  //     <img src="assets/images/category.png" className="menu-icon"></img>
+  //     <div className="header-title">Rival</div>
+  //     <img src="assets/rating-stars/100.png" className="logo"></img>
+  //   </div >)
 }
 
-// <div className="container">
-//       <ul className="horizontal">
-//         <li><Link to="/">Home</Link></li>
-//         {
-//           localStorage.getItem("jwt")
-//             ? <>
-//               <li><a onClick={
-//                 () => {
-//                   localStorage.removeItem("jwt")
-//                   window.location = "/"
-//                 }
-//               }>Log out</a></li>
-//               <li><Link to="/profile">My Profile</Link></li>
-//             </>
-//             : <>
-//               <li><Link to="/login">Login</Link></li>
-//               <li><Link to="/register">Register</Link></li>
-//             </>
-//         }
-
-//         <li><Link to="/rules">Rules</Link></li>
-//         <li><Link to="/faq">FAQ</Link></li>
-//       </ul>
-//     </div >
