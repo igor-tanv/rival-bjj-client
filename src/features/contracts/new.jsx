1ce10d2ecc8c7d26ac353fde5872f53ce7313bc8
Minh Phan <minhphan2603@gmail.com>
2020-12-17 22:39:13 +0700

    fix: player cannot challenge himself

--- src/features/contracts/new.jsx
+++ src/features/contracts/new.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react"
-import { withRouter } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Redirect, withRouter } from "react-router-dom";
 
-import { apiFetch } from "../../modules/api-fetch"
-import Form from "./form"
+import { apiFetch } from "../../modules/api-fetch";
+import Form from "./form";
 
-import weightClasses from "../../data/weight-classes.json"
-import matchTypes from "../../data/match-types.json"
+import weightClasses from "../../data/weight-classes.json";
+import matchTypes from "../../data/match-types.json";
 
 function isRequired(v) {
-  return v && v.length > 0 ? null : "is required"
+  return v && v.length > 0 ? null : "is required";
 }
 
 function isSelected(v) {
-  return v ? null : "is required"
+  return v ? null : "is required";
 }
 
 function didAgree(checked) {
-  return checked ? null : "must be agreed to"
+  return checked ? null : "must be agreed to";
 }
 
 const defaultValues = {
@@ -28,77 +28,98 @@ const defaultValues = {
   refereeName: "",
   type: null,
   ruleExceptions: "",
-  acceptsTos: false
-}
+  acceptsTos: false,
+};
 
 function IssueContract({ match }) {
   const [values, setValues] = useState({
-    ...defaultValues, playerId: localStorage.getItem("playerId"), opponentId: match.params.id
-  })
-  const [submitted, setSubmitted] = useState(false)
-  const [opponent, setOpponent] = useState({ firstName: "", lastName: "" })
-
+    ...defaultValues,
+    playerId: localStorage.getItem("playerId"),
+    opponentId: match.params.id,
+  });
+  const [submitted, setSubmitted] = useState(false);
+  const [opponent, setOpponent] = useState({ firstName: "", lastName: "" });
 
   const validations = {
     weightClass: [isSelected],
     type: [isSelected],
     location: [isRequired],
     refereeName: [isRequired],
-    acceptsTos: [didAgree]
-  }
+    acceptsTos: [didAgree],
+  };
 
-  const [errors, setErrors] = useState(Object.keys(validations).reduce((acc, key) => {
-    acc[key] = null
-    return acc
-  }, {}))
+  const [errors, setErrors] = useState(
+    Object.keys(validations).reduce((acc, key) => {
+      acc[key] = null;
+      return acc;
+    }, {})
+  );
 
   useEffect(() => {
-    apiFetch(`players/${match.params.id}`).then(json => setOpponent(json.player))
-  }, [])
-
+    apiFetch(`players/${match.params.id}`).then((json) =>
+      setOpponent(json.player)
+    );
+  }, []);
 
   function valid() {
-    return Object.keys(validations).flatMap(key => validations[key].map(v => v(values[key]))).every(e => e === null)
+    return Object.keys(validations)
+      .flatMap((key) => validations[key].map((v) => v(values[key])))
+      .every((e) => e === null);
   }
 
   function validate(key, validations) {
-    const errors = validations.map(v => v(values[key]))
-    setErrors(prev => ({
+    const errors = validations.map((v) => v(values[key]));
+    setErrors((prev) => ({
       ...prev,
-      [key]: errors
-    }))
+      [key]: errors,
+    }));
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     apiFetch(`contracts`, "post", {
       ...values,
-      startsAt: values.startsAt.valueOf()
-    }).then(json => {
-      setSubmitted(true)
-    }).catch(error => {
-      console.log(error)
+      startsAt: values.startsAt.valueOf(),
     })
+      .then((json) => {
+        setSubmitted(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  return <div>
-    <h1>Challenge Contract</h1>
+  if (values.playerId === values.opponentId)
+    return <Redirect to="/profile"></Redirect>;
 
-    {submitted ? <>
-      <h3>You have challenged {opponent.firstName} {opponent.lastName} to a match!</h3>
-      <h4>When they accept or reject you will be notified via email</h4>
-    </> : <Form
-        handleSubmit={handleSubmit}
-        opponent={opponent}
-        values={values}
-        setValues={setValues}
-        matchTypes={matchTypes}
-        weightClasses={weightClasses}
-        valid={valid}
-        errors={errors}
-        validate={validate}
-        validations={validations} />}
-  </div>
+  return (
+    <div>
+      <h1>Challenge Contract</h1>
+
+      {submitted ? (
+        <>
+          <h3>
+            You have challenged {opponent.firstName} {opponent.lastName} to a
+            match!
+          </h3>
+          <h4>When they accept or reject you will be notified via email</h4>
+        </>
+      ) : (
+        <Form
+          handleSubmit={handleSubmit}
+          opponent={opponent}
+          values={values}
+          setValues={setValues}
+          matchTypes={matchTypes}
+          weightClasses={weightClasses}
+          valid={valid}
+          errors={errors}
+          validate={validate}
+          validations={validations}
+        />
+      )}
+    </div>
+  );
 }
 
-export default withRouter(IssueContract)
+export default withRouter(IssueContract);
