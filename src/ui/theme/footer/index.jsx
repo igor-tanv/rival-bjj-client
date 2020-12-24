fedf9640aecff7bc6b1b5de9c6a3746a682978ed
Igor Tatarinov <igor.tanv@gmail.com>
2020-12-24 08:22:50 +0700

    footer update correct name

--- src/ui/theme/footer/index.jsx
+++ src/ui/theme/footer/index.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="rival__footer">
       <div className="rival__copy-right">
-        &#169; 2021 Copyright <span>bij-rival.com</span>
+        &#169; 2021 Copyright <span>rival-bjj.com</span>
       </div>
     </div>
   );
