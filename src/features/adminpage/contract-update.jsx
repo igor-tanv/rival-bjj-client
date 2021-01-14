ded5f9e194dab22b8c622d6f5b8b298f31b9cc7a
Igor Tatarinov <igor.tanv@gmail.com>
2021-01-14 16:15:00 +0700

    contract result submit form added

--- src/features/adminpage/contract-update.jsx
+++ src/features/adminpage/contract-update.jsx
@@ -7,6 +7,8 @@ import ContractTable from '../../ui/table'
 import Dropdown from "../../ui/dropdown"
 import TextField from "../../ui/text-field"
 
+import HorizontalList from "../../ui/horizontal-list"
+
 export default function ContractUpdate(contractObj) {
 
   const { contracts } = contractObj
@@ -16,7 +18,7 @@ export default function ContractUpdate(contractObj) {
   const [method, setMethod] = useState('')
   const [redRating, setRedRating] = useState('na')
   const [blueRating, setBlueRating] = useState('na')
-  const [refereeComments, setrefereeComments] = ('')
+  const [refereeComments, setrefereeComments] = useState('')
 
   const winnerOptions = ["red", "blue", "draw", "cancelled"]
   const winMethod = ["Sub", "Points", "DQ", "Forfeit", "Injury", "Advantage"]
@@ -37,8 +39,71 @@ export default function ContractUpdate(contractObj) {
       });
   }
 
-  return <>
-    <ContractTable
+  function renderContracts(contract) {
+    return <form onSubmit={handleSubmit}>
+      <tr>
+        <th>Winner</th>
+        <th>Method</th>
+        <th>Location</th>
+        <th>Match type</th>
+        <th>Red Corner</th>
+        <th>Red QR</th>
+        <th>Blue Corner</th>
+        <th>Blue QR</th>
+        <th>Referee Comments</th>
+        <th></th>
+      </tr>
+      <td>
+        <Dropdown
+          options={winnerOptions}
+          onChange={setWinner}
+          value={winner}
+        />
+      </td>
+      <td>
+        <Dropdown
+          options={winMethod}
+          onChange={setMethod}
+          value={method}
+        />
+      </td>
+      <td>{contract.location}</td>
+      <td>{contract.type}</td>
+      <td>{contract.playerFirstName} {contract.playerLastName}</td>
+      <td>
+        <Dropdown
+          options={qualityRating}
+          onChange={setRedRating}
+          value={redRating}
+        />
+      </td>
+      <td>{contract.opponentFirstName} {contract.opponentLastName}</td>
+      <td>
+        <Dropdown
+          options={qualityRating}
+          onChange={setBlueRating}
+          value={blueRating}
+        />
+      </td>
+      <td>
+        <TextField
+          value={refereeComments}
+          onChange={(val) => setrefereeComments(val)}
+        />
+      </td>
+
+      <Button type="submit" onClick={() => setContractId(contract.id)}>Submit Result</Button>
+    </form>
+  }
+
+  return <HorizontalList
+    items={contracts}
+    renderItem={renderContracts}
+  />
+
+}
+
+{/* <ContractTable
       data={contracts}
       renderHead={() => {
         return (
@@ -103,6 +168,4 @@ export default function ContractUpdate(contractObj) {
         );
       }}
     />
-  </>
-
-}
+  </> */}
