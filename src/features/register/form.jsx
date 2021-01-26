622e1d5876de8bffb6c84b8017e2f73fb0e86192
Igor Tatarinov <igor.tanv@gmail.com>
2021-01-26 11:57:27 +0700

    Resize the image before uploading

--- src/features/register/form.jsx
+++ src/features/register/form.jsx
@@ -67,14 +67,47 @@ export default function RegisterForm({ setComplete }) {
   function imageChanged() {
     const file = fileRef.current && fileRef.current.files[0];
     if (!file) return;
-    const reader = new FileReader();
+
+    // Load the image
+    var reader = new FileReader();
+    reader.onload = function (readerEvent) {
+      var image = new Image();
+      image.onload = function (imageEvent) {
+
+        // Resize the image
+        const canvas = document.createElement('canvas')
+        const max_size = 140
+        let width = image.width
+        let height = image.height
+
+        if (width > height) {
+          if (width > max_size) {
+            height *= max_size / width;
+            width = max_size;
+          }
+        } else {
+          if (height > max_size) {
+            width *= max_size / height;
+            height = max_size;
+          }
+        }
+
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+
+        const resizedImage = canvas.toDataURL('image/jpeg')
+
+        //image.src = readerEvent.target.result;
+
+        setValues((prev) => ({
+          ...prev,
+          avatar: resizedImage,
+        }));
+      }
+      image.src = readerEvent.target.result;
+    }
     reader.readAsDataURL(file);
-    reader.onloadend = function (e) {
-      setValues((prev) => ({
-        ...prev,
-        avatar: reader.result,
-      }));
-    };
   }
 
   function handleSubmit(e) {
