a3fbcb2179649e8f2eaf48f8d7841aa45671189e
Igor Tatarinov <igor.tanv@gmail.com>
2021-01-27 09:11:03 +0700

    update avatar

--- src/features/profile/form.jsx
+++ src/features/profile/form.jsx
@@ -36,18 +36,18 @@ export default function EditProfileForm({ player }) {
   });
   const fileRef = useRef();
 
-  function imageChanged() {
-    const file = fileRef.current && fileRef.current.files[0];
-    if (!file) return;
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = function (e) {
-      setValues((prev) => ({
-        ...prev,
-        avatar: reader.result,
-      }));
-    };
-  }
+  // function imageChanged() {
+  //   const file = fileRef.current && fileRef.current.files[0];
+  //   if (!file) return;
+  //   const reader = new FileReader();
+  //   reader.readAsDataURL(file);
+  //   reader.onloadend = function (e) {
+  //     setValues((prev) => ({
+  //       ...prev,
+  //       avatar: reader.result,
+  //     }));
+  //   };
+  // }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -86,6 +86,52 @@ export default function EditProfileForm({ player }) {
       .catch((error) => {
         debugger;
       });
+  }
+
+  function imageChanged() {
+    const file = fileRef.current && fileRef.current.files[0];
+    if (!file) return;
+
+    // Load the image
+    var reader = new FileReader();
+    reader.onload = function (readerEvent) {
+      var image = new Image();
+      image.onload = function (imageEvent) {
+
+        // Resize the image
+        const canvas = document.createElement('canvas')
+        const max_size = 140
+        let width = image.width
+        let height = image.height
+
+        if (width > height) {
+          if (width > max_size) {
+            height *= max_size / width;
+            width = max_size;
+          }
+        } else {
+          if (height > max_size) {
+            width *= max_size / height;
+            height = max_size;
+          }
+        }
+
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+
+        const resizedImage = canvas.toDataURL('image/jpeg')
+
+        //image.src = readerEvent.target.result;
+
+        setValues((prev) => ({
+          ...prev,
+          avatar: resizedImage,
+        }));
+      }
+      image.src = readerEvent.target.result;
+    }
+    reader.readAsDataURL(file);
   }
 
   return (
@@ -191,8 +237,8 @@ export default function EditProfileForm({ player }) {
                 <img src={values.avatar} alt="" width={100} />{' '}
               </div>
             ) : (
-              ''
-            )}
+                ''
+              )}
             <div>
               <input type="file" ref={fileRef} onChange={imageChanged} />
             </div>
